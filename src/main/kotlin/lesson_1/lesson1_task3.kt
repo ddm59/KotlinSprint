3809e9e7e30b8420ec54909df3e7deaa0a895953
guilt <gulilty4all@gmail.com>
2024-11-14 09:47:33 +0600

    KS-1-3 Refactor

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -3,17 +3,6 @@ package org.example.lesson_1
 import jdk.internal.misc.Signal
 
 fun main(){
-    val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
 
-    println("Первый полет человека в Космос.")
-    println("Год полета - $year")
-    println("Взлет - "+String.format("%02d:%02d",hour,minute))
-
-    hour = 10
-    minute = 55
-
-    println("Посадка - "+String.format("%02d:%02d",hour,minute))
 
 }
