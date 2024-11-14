eae78d90ceca697815eb48cee8eed69f7ab200b9
guilt <gulilty4all@gmail.com>
2024-11-14 21:09:37 +0600

    KS-2-4 fixed issues

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,5 +1,10 @@
 package org.example.lesson_2
 
-fun main(){
-    println((3.0+4.0+3.0+5.0)/4)
+fun main() {
+    val firstStudentMark = 3.0
+    val secondStudentMark = 4.0
+    val thirdStudentMark = 3.0
+    val fourthStudentMark = 5.0
+    val studentCount = 4
+    println((firstStudentMark + secondStudentMark + thirdStudentMark + fourthStudentMark) / studentCount)
 }
