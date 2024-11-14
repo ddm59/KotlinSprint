2565a19869343c4ec77f282e1f3a6a1585e83c8b
guilt <gulilty4all@gmail.com>
2024-11-14 21:54:36 +0600

    KS-2-2 fixed issues

--- src/main/kotlin/lesson2_2/lesson2_task2.kt
+++ src/main/kotlin/lesson2_2/lesson2_task2.kt
@@ -1,11 +1,15 @@
 package org.example.lesson2_2
 
-fun main(){
-    var regularEmployeesSalary: Int = 50*30000
-    var totalSalary: Int = 30*20000+regularEmployeesSalary
-    var averageSalary: Int = totalSalary/80
+fun main() {
+    val numberOfEmployee: Int = 50
+    val numberOfTrainee: Int = 30
+    val employeeSalary: Int = 30000
+    val traineeSalary: Int = 20000
+    var employeeExpenses: Int = numberOfEmployee * employeeSalary
+    var totalSalaryExpenses: Int = numberOfTrainee * traineeSalary + employeeExpenses
+    var averageSalary: Int = totalSalaryExpenses / (numberOfEmployee + numberOfTrainee)
 
-    println("Расходы на выплату зарплаты постоянных сотрудников: $regularEmployeesSalary")
-    println("Общие расходы по ЗП после прихода стажеров: $totalSalary")
+    println("Расходы на выплату зарплаты постоянных сотрудников: $employeeExpenses")
+    println("Общие расходы по ЗП после прихода стажеров: $totalSalaryExpenses")
     println("Среднюю ЗП одного сотрудника после устройства стажеров: $averageSalary")
 }
