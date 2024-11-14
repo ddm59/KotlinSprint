31012e35e2e9d1ebe5bd2c5b60331a291dea0ee3
guilt <gulilty4all@gmail.com>
2024-11-14 11:56:00 +0600

    KS-1-4 Initialized 6 variables.organized output

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_1
+
+fun main() {
+
+    //Обьявление и инициализация переменных
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    //Вывод переменных в консоль
+    println("length = $length")
+    println("age = $age")
+    println("partOfDay = $partOfDay")
+    println("seconds = $seconds")
+    println("partOfYear = $partOfYear")
+    println("apogee = $apogee")
+}
