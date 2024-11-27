9ee36357d9c9c7440234a661a2759ebb25b2329f
Dmitry <guilty4all@gmail.com>
2024-11-27 19:22:23 +0600

    KS-1-5 refactoring code. add 2 const val

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,14 +1,19 @@
 package org.example.lesson_1
 
+const val SECONDS_IN_MINUTE = 60
+const val SECONDS_IN_HOUR = 3600
 fun main() {
 
-    val timeInSpaceInSeconds: Int = 6480
-    val secondsInSpace: Int = timeInSpaceInSeconds % 60
-    val minutesInSpace: Int = (timeInSpaceInSeconds / 60) % 60
-    val hoursInSpace: Int = timeInSpaceInSeconds / 3600
+    var timeInSpaceInSeconds = 6480
+    val secondsInSpace: Int = timeInSpaceInSeconds % SECONDS_IN_MINUTE
+    val minutesInSpace: Int = (timeInSpaceInSeconds / SECONDS_IN_MINUTE) % SECONDS_IN_MINUTE
+    val hoursInSpace: Int = timeInSpaceInSeconds / SECONDS_IN_HOUR
 
 
-    println(String.format(
-            "Время проведенное в космосе: %02d:%02d:%02d", hoursInSpace,  minutesInSpace, secondsInSpace))
+    println(
+        String.format(
+            "Время проведенное в космосе: %02d:%02d:%02d", hoursInSpace, minutesInSpace, secondsInSpace
+        )
+    )
 
 }
