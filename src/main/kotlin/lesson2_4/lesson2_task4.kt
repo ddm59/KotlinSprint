673a9b14375cb08bc5cd6a7ceea6a1a768a2ac37
Dmitry <guilty4all@gmail.com>
2024-11-27 19:58:46 +0600

    KS-2-4 refactoring code. fix issues

--- src/main/kotlin/lesson2_4/lesson2_task4.kt
+++ src/main/kotlin/lesson2_4/lesson2_task4.kt
@@ -1,13 +1,13 @@
 package org.example.lesson2_4
 
 fun main() {
-    val crystalOre: Int = 7
-    val ironOre: Int = 11
-
+    var crystalOre: Int = 7
+    var ironOre: Int = 11
+    var percentModifier: Float = 0.2f
     println("Колличество добытых ресурсов: ")
     println("----Кристалическая руда: $crystalOre")
     println("----Железная руда: $ironOre")
     println("Бафф +20% к добытым ресурсам: ")
-    println("----Кристалическая руда: " + (crystalOre * 0.2).toInt())
-    println("----Железная руда: " + (ironOre * 0.2).toInt())
+    println("----Кристалическая руда: " + (crystalOre * percentModifier).toInt())
+    println("----Железная руда: " + (ironOre * percentModifier).toInt())
 }
