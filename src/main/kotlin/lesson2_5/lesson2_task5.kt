ee24d9ab25331039edcdafbe8cdfa91d3828de97
Dmitry <guilty4all@gmail.com>
2024-11-28 14:49:57 +0600

    KS-2-5 fix some issues

--- src/main/kotlin/lesson2_5/lesson2_task5.kt
+++ src/main/kotlin/lesson2_5/lesson2_task5.kt
@@ -3,13 +3,13 @@ package org.example.lesson2_5
 import kotlin.math.pow
 
 fun main() {
-    val initialAmount = 70000.0
-    val yearPercentRate = 16.7 / 100
+    val initialAmount = 70000
+    val yearPercentRate = 16.7
     val periodInYears = 20
     val numberOfChargesInYear = 1
 
     val result =
-        initialAmount * (1 + yearPercentRate / numberOfChargesInYear).pow(numberOfChargesInYear * periodInYears)
+        initialAmount.toFloat() * (1 + (yearPercentRate / 100) / numberOfChargesInYear).pow(numberOfChargesInYear * periodInYears)
 
     println(String.format("%.3f", result))
 
