21be2de90937f79da417a44e53dbf3f8110cc07f
Dmitry <guilty4all@gmail.com>
2024-11-28 13:52:06 +0600

    KS-2-3 reformat code

--- src/main/kotlin/lesson2_3/lesson2_task3.kt
+++ src/main/kotlin/lesson2_3/lesson2_task3.kt
@@ -2,13 +2,13 @@ package org.example.lesson2_3
 
 const val MINUTES_IN_HOUR: Int = 60
 
-fun main(){
+fun main() {
     var startHour: Int = 9
     var startMinute: Int = 39
     var travelTimeInMinutes: Int = 457
-    var departureTime: Int = (startHour* MINUTES_IN_HOUR+startMinute)
-    var arrivalHour: Int = (departureTime+travelTimeInMinutes)/MINUTES_IN_HOUR
-    var arrivalMinute: Int = (departureTime+travelTimeInMinutes)%MINUTES_IN_HOUR
+    var departureTime: Int = (startHour * MINUTES_IN_HOUR + startMinute)
+    var arrivalHour: Int = (departureTime + travelTimeInMinutes) / MINUTES_IN_HOUR
+    var arrivalMinute: Int = (departureTime + travelTimeInMinutes) % MINUTES_IN_HOUR
 
-    println("Время прибытия поезда: "+String.format("%02d:%02d", arrivalHour, arrivalMinute))
+    println("Время прибытия поезда: " + String.format("%02d:%02d", arrivalHour, arrivalMinute))
 }
