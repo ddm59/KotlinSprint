d1f11c7dba4ce2596119cceb7d9b847b505772fe
Dmitry <guilty4all@gmail.com>
2024-11-29 09:54:41 +0600

    KS-3-5- rename the branch. refactored code

--- src/main/kotlin/lesson3_5/lesson3_task5.kt
+++ src/main/kotlin/lesson3_5/lesson3_task5.kt
@@ -1,14 +1,20 @@
 package org.example.lesson3_5
 
 fun main() {
-    var moveData = "D2-D4;0"
+    val moveData = "D2-D4;0"
 
-    var startPossition = moveData.split("-", ";")[0]
-    var nextPossition = moveData.split("-", ";")[1]
-    var moveNumber = moveData.split("-", ";")[2].toInt()
+    //var startPossition = moveData.split("-", ";")[0]
+    //var nextPossition = moveData.split("-", ";")[1]
+    //var moveNumber = moveData.split("-", ";")[2].toInt()
 
-    println(startPossition)
-    println(nextPossition)
-    println(moveNumber)
+    //println(startPossition)
+    //println(nextPossition)
+    //println(moveNumber)
+
+    var possitionList = moveData.split("-", ";")
+
+    println(possitionList[0])
+    println(possitionList[1])
+    println(possitionList[2])
 
 }
