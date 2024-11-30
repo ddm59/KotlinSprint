b6cfb1c2f5883049a9bbf457507a07e9666cd131
Dmitry <guilty4all@gmail.com>
2024-11-30 18:26:40 +0600

    KS-4-5- moved logic from output in to variable added console input

--- src/main/kotlin/lesson4_5/lesson4_task5.kt
+++ src/main/kotlin/lesson4_5/lesson4_task5.kt
@@ -3,8 +3,11 @@ package org.example.lesson4_5
 fun main() {
 
     println("\nДиагностика корабля. Требуется ввести текущие данные: \n")
+    //Не до конца понял как правильно осуществить провверку на ввод значений оперируя текущими вводными данными
+    // так как по идее мы еще не прошли условные операторы сделал через ?:  чтоб хотя бы избежать ввода пустых значений
+    // в идеале нужно проверять ввод на корректный ввод
+    // возможно не верно понял ТЗ.
     print("Наличие повреждений на корабле?  (true\\false ) :   ")
-
     var shipDamage = readlnOrNull() ?: "true"
     print("Колличество экипажа на кобале. (кол-во человек) :   ")
     var shipCrew = readlnOrNull() ?: "0"
@@ -13,11 +16,10 @@ fun main() {
     print("Наличие благоприятной погоды (true\\false ):   ")
     var weather = readlnOrNull() ?: "false"
 
-    println(
-        "Разрешение на отплытие :  ${
-            (!shipDamage.toBoolean() && (shipCrew.toInt() >= 55 && shipCrew.toInt() <= 70) && (shipProvision.toInt() > 50) && weather.toBoolean())
-                    || ((shipCrew.toInt() == 70) && (shipProvision.toInt() >= 50) && weather.toBoolean())
-        }"
-    )
+    var isReadyForSail =
+        (!shipDamage.toBoolean() && (shipCrew.toInt() >= 55 && shipCrew.toInt() <= 70) && (shipProvision.toInt() > 50) && weather.toBoolean())
+                || ((shipCrew.toInt() == 70) && (shipProvision.toInt() >= 50) && weather.toBoolean())
+
+    println("Разрешение на отплытие :  $isReadyForSail")
 
 }
