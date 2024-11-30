1618def89a1ae559fe9be7ca01f2de9a69afa2de
Dmitry <guilty4all@gmail.com>
2024-11-30 17:35:50 +0600

    KS-3-5- refactor project

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -3,14 +3,6 @@ package org.example.lesson_3
 fun main() {
     val moveData = "D2-D4;0"
 
-    //var startPossition = moveData.split("-", ";")[0]
-    //var nextPossition = moveData.split("-", ";")[1]
-    //var moveNumber = moveData.split("-", ";")[2].toInt()
-
-    //println(startPossition)
-    //println(nextPossition)
-    //println(moveNumber)
-
     var possitionList = moveData.split("-", ";")
 
     println(possitionList[0])
