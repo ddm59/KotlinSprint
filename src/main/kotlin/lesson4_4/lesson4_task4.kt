47e7e24340cfd9f19dc9307f9a4ff2a6655aca10
Dmitry <guilty4all@gmail.com>
2024-11-30 18:13:24 +0600

    KS-4-4- moved logic from output in to variable

--- src/main/kotlin/lesson4_4/lesson4_task4.kt
+++ src/main/kotlin/lesson4_4/lesson4_task4.kt
@@ -3,14 +3,15 @@ package org.example.lesson4_4
 fun main() {
 
     var dayCount = 5
+    var isOddDay = dayCount % 2 != 0
+    var isEvenDay = dayCount % 2 == 0
 
     println(
         """
-        |Упражнения для рук:    ${(dayCount % 2 != 0)}
-        |Упражнения для ног:    ${(dayCount % 2 == 0)}
-        |Упражнения для спины:  ${(dayCount % 2 == 0)}
-        |Упражнения для пресса: ${(dayCount % 2 != 0)}
-        
+        |Упражнения для рук:    $isOddDay
+        |Упражнения для ног:    $isEvenDay
+        |Упражнения для спины:  $isEvenDay
+        |Упражнения для пресса: $isOddDay
     """.trimMargin()
     )
 }
