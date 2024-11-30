25034d7da1fd692302927281d8e1b6f43792e2bd
Dmitry <guilty4all@gmail.com>
2024-11-30 19:43:03 +0600

    KS-5-2- checking user majority show content accepted by majority check

---
+++ src/main/kotlin/lesson5_2/lesson5_task2.kt
@@ -0,0 +1,20 @@
+package org.example.lesson5_2
+
+const val MAJORITY_AGE = 18
+const val CURRENT_YEAR = 2024
+
+fun main() {
+
+    println("Введите год вашего рождения: ")
+
+    val userBirthDate = readln().toInt()
+    val userAge = CURRENT_YEAR - userBirthDate
+
+    if (userAge >= MAJORITY_AGE) {
+        println("Показать экран со скрытым контентом")
+    } else {
+        println("Показать экран с общедоступным контентом")
+    }
+
+
+}
