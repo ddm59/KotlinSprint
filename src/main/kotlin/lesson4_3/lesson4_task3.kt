925958c52b196c7c3e1d2a13f25addd1d5bbdb19
Dmitry <guilty4all@gmail.com>
2024-12-01 16:30:15 +0600

    KS-4-3- fix issues

--- src/main/kotlin/lesson4_3/lesson4_task3.kt
+++ src/main/kotlin/lesson4_3/lesson4_task3.kt
@@ -1,18 +1,22 @@
 package org.example.lesson4_3
-
-const val REQUIRED_WEATER: String = "солнечная"
+//Требуемые значения. неизменяемые константы
+const val REQUIRED_WEATHER: String = "солнечная"
 const val REQUIRED_TENT_POSSITION: String = "открыт"
 const val REQUIRED_HUMIDITY: Int = 20
 const val BAD_SEASON: String = "зима"
 
 fun main() {
-
-    var weatherCheck = REQUIRED_WEATER == "солнечная"
-    var tentCheck = REQUIRED_TENT_POSSITION == "открыт"
+    // входные значения
     var currentHumidity = 20
     var currentSeason = "зима"
+    var currentWeather = "солнечная"
+    var currentTentPosition = "открыт"
+    //блок с проверками
+    var weatherCheck = REQUIRED_WEATHER == currentWeather
+    var tentCheck = REQUIRED_TENT_POSSITION == currentTentPosition
     var conditionCheck = weatherCheck && tentCheck && (currentHumidity == REQUIRED_HUMIDITY) && (currentSeason != BAD_SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых?: $conditionCheck")
+
 
 }
