53728151180a45c8215d329b43b615e6eb8989fa
Dmitry <guilty4all@gmail.com>
2024-12-01 16:36:31 +0600

    KS-4-4- fix issues

--- src/main/kotlin/lesson4_4/lesson4_task4.kt
+++ src/main/kotlin/lesson4_4/lesson4_task4.kt
@@ -4,13 +4,12 @@ fun main() {
 
     var dayCount = 5
     var isOddDay = dayCount % 2 != 0
-    var isEvenDay = dayCount % 2 == 0
 
     println(
         """
         |Упражнения для рук:    $isOddDay
-        |Упражнения для ног:    $isEvenDay
-        |Упражнения для спины:  $isEvenDay
+        |Упражнения для ног:    ${!isOddDay}
+        |Упражнения для спины:  ${!isOddDay}
         |Упражнения для пресса: $isOddDay
     """.trimMargin()
     )
