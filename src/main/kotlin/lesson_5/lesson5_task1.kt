56e5ceecd09159cd7f8da6935d0dc061d29a8208
Dmitry <guilty4all@gmail.com>
2024-12-01 17:01:12 +0600

    KS-5-1- fix issues

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_5
-
+const val FIRST_CAPTCHA_NUM: Int = 350
+const val SECOND_CAPTCHA_NUM: Int = 35
 
 fun main() {
 
     println("Для продолжения авторизации необходимо пройти проверку....")
-    println("Чему равен результат выражения ( 350 + 35 ) ?")
+    println("Чему равен результат выражения ( $FIRST_CAPTCHA_NUM + $SECOND_CAPTCHA_NUM ) ?")
 
     val answer = readln().toInt()
 
-    if (answer == 385) {
+    if (answer == (FIRST_CAPTCHA_NUM+ SECOND_CAPTCHA_NUM)) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещен!!!")
