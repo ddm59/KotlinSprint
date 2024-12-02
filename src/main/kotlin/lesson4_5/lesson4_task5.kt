53de649249949a402fc13bba88e99e651611d6ef
Dmitry <guilty4all@gmail.com>
2024-12-02 10:02:55 +0600

    KS-4-5-fix issues add constants for checks

--- src/main/kotlin/lesson4_5/lesson4_task5.kt
+++ src/main/kotlin/lesson4_5/lesson4_task5.kt
@@ -3,22 +3,24 @@ package org.example.lesson4_5
 const val REQUIRED_CREW: Int = 70
 const val MIN_REQUIRED_CREW: Int = 55
 const val REQUIRED_PROVISION: Int = 50
+const val REQUIRED_WEATHER: String = "good"
+const val IS_SHIP_DAMAGED: String = "no"
 
 fun main() {
 
     println("\nДиагностика корабля. Требуется ввести текущие данные: \n")
-    print("Наличие повреждений на корабле?  (true\\false ) :   ")
-    var shipDamage = readln().toBoolean()
+    print("Наличие повреждений на корабле?  (yes\\no ) :   ")
+    var shipDamageCheck = readln() == IS_SHIP_DAMAGED
     print("Колличество экипажа на кобале. (кол-во человек) :   ")
     var shipCrew = readln().toInt()
     print("Количесво провизии на корабле (кол-во шт.) :   ")
     var shipProvision = readln().toInt()
-    print("Наличие благоприятной погоды (true\\false ):   ")
-    var weather = readln().toBoolean()
+    print("Наличие благоприятной погоды (good\\bad ):   ")
+    var weatherCheck = readln() == REQUIRED_WEATHER
 
     var isReadyForSail =
-        (!shipDamage&& (shipCrew>= MIN_REQUIRED_CREW && shipCrew<= REQUIRED_CREW) && (shipProvision> REQUIRED_PROVISION) && weather)
-                || ((shipCrew== REQUIRED_CREW) && (shipProvision >= REQUIRED_PROVISION) && weather)
+        (shipDamageCheck&& (shipCrew>= MIN_REQUIRED_CREW && shipCrew<= REQUIRED_CREW) && (shipProvision> REQUIRED_PROVISION) && weatherCheck)
+                || ((shipCrew== REQUIRED_CREW) && (shipProvision >= REQUIRED_PROVISION) && weatherCheck)
 
     println("Разрешение на отплытие :  $isReadyForSail")
 
