6cb6d1c49af9486237d9c9522b41b8c3e33c531e
Dmitry <guilty4all@gmail.com>
2024-12-03 12:39:43 +0600

    KS-6-3- some output fix

--- src/main/kotlin/lesson6_3/lesson6_task3.kt
+++ src/main/kotlin/lesson6_3/lesson6_task3.kt
@@ -1,12 +1,12 @@
 package org.example.lesson6_3
 
 fun main() {
-    println("Введите кол-во секунд которые нужно засечь")
+    print("Введите кол-во секунд которые нужно засечь")
     var timeInSeconds = readln().toInt()
 
     while (--timeInSeconds > 0) {
         Thread.sleep(1000)
-        print("Осталось секунд: ${timeInSeconds}")
+        println("Осталось секунд: ${timeInSeconds}")
     }
     println("Время вышло!!!")
 }
