a8d7195cde528ee2ac37a067a5c26aa771db9370
Dmitry <guilty4all@gmail.com>
2024-12-04 15:16:19 +0600

    KS-7-4 timer with back count

---
+++ src/main/kotlin/lesson7_4/lesson7_task4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson7_4
+
+fun main() {
+    print("Введите кол-во засекаемых секунд: ")
+    var userTime = readln().toInt()
+
+    for (i in userTime downTo 1) {
+        Thread.sleep(1000)
+        if (i == 1) break
+        println("Осталось: ${i - 1} секунд")
+    }
+    println("Время вышло!!!")
+}
