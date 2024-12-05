467a3bc0863433132170e5311bbbf055d2547f2d
Dmitry <guilty4all@gmail.com>
2024-12-05 15:30:48 +0600

    KS-8-3 ingredients search function without loop

---
+++ src/main/kotlin/lesson8_3/lesson8_task3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson8_3
+
+fun main() {
+    val recipeIngredients = arrayOf("Перец", "Помидоры","Чеснок"," Картофель", "Лук", "Говядина")
+
+    print("Введите название ингредиента для поиска:  ")
+    var ingredientName = readln()
+    var ingredientIndex = 0
+
+    if (recipeIngredients.contains(ingredientName)) {
+
+        ingredientIndex = recipeIngredients.indexOf(ingredientName)
+        println("Ингредиент [${recipeIngredients[ingredientIndex]}] в рецепте есть")
+
+    }else{
+        println("Такого ингредиента в рецепте нет")
+    }
+}
