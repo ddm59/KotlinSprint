2732ffc2f3578954b25e000df154b95f463224b3
Dmitry <guilty4all@gmail.com>
2024-12-05 11:21:18 +0600

    KS-5-7 fix issues

--- src/main/kotlin/lesson5_7/lesson5_task7.kt
+++ src/main/kotlin/lesson5_7/lesson5_task7.kt
@@ -6,21 +6,24 @@ fun main() {
 
     println("Для расчета стоимости поездки введите следующие данные.")
     println("Введите расстояние поездки в (километрах): ")
-    var tripDistance = readln().toFloat()
+
+    val tripDistance = readln().toFloat()
+
     println("Расход топлива на 100 км (в литрах): ")
-    var fuelСonsumption = readln().toFloat()
+
+    val fuelСonsumption = readln().toFloat()
+
     println("Текущая цена за литр топлива: ")
+
     var fuelPrice = readln().toFloat()
 
     var amountOfFuel = (tripDistance * fuelСonsumption) / FORMULA_DIVISOR
     var tripCost = amountOfFuel * fuelPrice
+
     println(
         String.format(
             "Кол-во топлива требуемое на поездку: %.2f л.\nИтоговая стоимость поездки: %.2f р.",
             amountOfFuel,
             tripCost
-        )
-    )
-
-
+        ))
 }
