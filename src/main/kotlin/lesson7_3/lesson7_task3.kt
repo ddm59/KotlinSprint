fea44aa7e2666911dda6f7587e4e9de009afd178
Dmitry <guilty4all@gmail.com>
2024-12-06 08:30:16 +0600

    KS-5-7 refactor

--- src/main/kotlin/lesson7_3/lesson7_task3.kt
+++ src/main/kotlin/lesson7_3/lesson7_task3.kt
@@ -3,7 +3,7 @@ package org.example.lesson7_3
 fun main() {
 
     print("Введите верхнюю границу диапазона: ")
-    var upperLimit = readln().toInt()
+    val upperLimit = readln().toInt()
 
     for (i in 2..upperLimit step 2) {
         println("Четное число из диапазона: $i")
