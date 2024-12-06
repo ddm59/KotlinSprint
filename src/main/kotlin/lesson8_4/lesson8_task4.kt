d8ae2bb65d3df257b86ef343dd97ccb7a4965b70
Dmitry <guilty4all@gmail.com>
2024-12-06 13:21:20 +0600

    KS-8-4 fix issues

--- src/main/kotlin/lesson8_4/lesson8_task4.kt
+++ src/main/kotlin/lesson8_4/lesson8_task4.kt
@@ -2,23 +2,22 @@ package org.example.lesson8_4
 
 fun main() {
 
-    val recipeIngredients = arrayOf("лук","сыр","помидор","масло","картофель")
-    var newIngredient =""
-    var ingredientIndex= 0
+    val recipeIngredients = arrayOf("лук", "сыр", "помидор", "масло", "картофель")
+    var newIngredient = ""
 
     println(recipeIngredients.contentToString())
     print("Введите ингредиент для замены: ")
 
     val ingredientToChange = readln()
+    val ingredientIndex = recipeIngredients.indexOf(ingredientToChange)
 
-    if (recipeIngredients.contains(ingredientToChange)) {
-        ingredientIndex =recipeIngredients.indexOf(ingredientToChange)
+    if (ingredientIndex >= 0) {
 
         print("Введите новый ингредиент: ")
         newIngredient = readln()
 
-        recipeIngredients[ingredientIndex]= newIngredient
-    }else{
+        recipeIngredients[ingredientIndex] = newIngredient
+    } else {
         println("Ингредиент не найден")
     }
     println(recipeIngredients.contentToString())
