643f61c7d324008e34f5d6953e10ab520b6afa16
Dmitry <guilty4all@gmail.com>
2024-12-07 19:37:56 +0600

    KS-8-3 some fixes

--- src/main/kotlin/lesson8_3/lesson8_task3.kt
+++ src/main/kotlin/lesson8_3/lesson8_task3.kt
@@ -5,12 +5,9 @@ fun main() {
 
     print("Введите название ингредиента для поиска:  ")
     var ingredientName = readln()
-    var ingredientIndex = 0
 
     if (ingredientName in recipeIngredients) {
-
-        ingredientIndex = recipeIngredients.indexOf(ingredientName)
-        println("Ингредиент [${recipeIngredients[ingredientIndex]}] в рецепте есть")
+        println("Ингредиент [${ingredientName}}] в рецепте есть")
 
     }else{
         println("Такого ингредиента в рецепте нет")
