6ae56b3d2ba353ff16b76fa7cd0c05ad5728aa8c
Dmitry <guilty4all@gmail.com>
2024-12-07 20:06:21 +0600

    KS-8-5 lambda array initialization

--- src/main/kotlin/lesson8_5/lesson8_task5.kt
+++ src/main/kotlin/lesson8_5/lesson8_task5.kt
@@ -3,13 +3,10 @@ package org.example.lesson8_5
 fun main() {
 
     print("Введи кол-во ингредиентов в рецепте:  ")
-    val recipeSize = readln().toInt()
-    var recipeIngridients: Array<String> = Array(recipeSize) { "" }
 
-    for (i in 0..recipeSize - 1) {
-
-        print("Ингредиент №${i + 1}\nВведите наименование ингредиента: ")
-        recipeIngridients[i] = readln()
+    var recipeIngridients: Array<String> = Array(readln().toInt()) {
+        println("Введите наименование ингредиента: ")
+        readln()
     }
     println("Состав введенного рецепта: ${recipeIngridients.joinToString(", ")}")
 }
