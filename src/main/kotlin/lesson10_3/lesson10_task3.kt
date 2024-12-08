15f2bf9cc5d36de51d6c8223bdab4ef25586a836
Dmitry <guilty4all@gmail.com>
2024-12-08 20:56:32 +0600

    KS-10-3  fixed issues

--- src/main/kotlin/lesson10_3/lesson10_task3.kt
+++ src/main/kotlin/lesson10_3/lesson10_task3.kt
@@ -2,23 +2,20 @@ package org.example.lesson10_3
 
 fun main() {
     print("Введите требуемую длину пароля: ")
-    println(pwdGenerator(readln().toInt()))
+    println(generatePassword(readln().toInt()))
 }
 
-fun pwdGenerator(pwdLen: Int): String {
-    var symbolRange = listOf('!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ' ')
-    var intRange = ('0'..'9').toList()
-    var generatedPwd = mutableListOf<Char>()
+fun generatePassword(pwdLen: Int): String {
+    var symbolRange = (' '..'/')
+    var intRange = ('0'..'9')
+    var generatedPwd = ""
 
     for (i in 1..pwdLen) {
-
         if (i % 2 != 0) {
-            intRange.shuffled()
-            generatedPwd.add(intRange.random())
+            generatedPwd += intRange.random()
         } else {
-            symbolRange.shuffled()
-            generatedPwd.add(symbolRange.random())
+            generatedPwd += symbolRange.random()
         }
     }
-    return generatedPwd.joinToString("")
+    return generatedPwd
 }
