4d999c515bbeca676b7cd0269fd772210f5853cf
Dmitry <guilty4all@gmail.com>
2024-12-10 19:45:35 +0600

    KS-11-1 simple class with fields

---
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_11
+
+fun main() {
+    val user1 = User(
+        id = 1,
+        login = "looney",
+        password = "Qwerty2024",
+        email = "looney2024@gmail.com",
+    )
+
+    val user2 = User(
+        id = 2,
+        login = "Johny",
+        password = "QET123adg!!",
+        email = "johnyboy@mail.ru",
+    )
+    println("Данные user1")
+    println("ID: ${user1.id}\nLogin: ${user1.login}\nPassword: ${user1.password}\nEmail: ${user1.email}")
+    println("Данные user2")
+    println("ID: ${user2.id}\nLogin: ${user2.login}\nPassword: ${user2.password}\nEmail: ${user2.email}")
+}
