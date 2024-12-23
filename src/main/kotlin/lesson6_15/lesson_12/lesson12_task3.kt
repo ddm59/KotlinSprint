2783a86abe5f04905d67e321154bf782d7e437bb
Dmitry <guilty4all@gmail.com>
2024-12-23 19:16:48 +0600

    KS-12-3 refactor

--- src/main/kotlin/lesson6_15/lesson_12/lesson12_task3.kt
+++ src/main/kotlin/lesson6_15/lesson_12/lesson12_task3.kt
@@ -1,15 +1,15 @@
 package org.example.lesson6_15.lesson_12
 
-const val KELVIN_CONSTANT = 273
+const val KELVIN_CONSTANT1 = 273
 
 fun main() {
-    val temp = TempDaily(298, 288, true)
+    val temp = TempDaily1(298, 288, true)
     temp.getWeatherInfo()
 }
 
-class TempDaily(_dayTemperature: Int, _nightTemperature: Int, _precipitationExpected: Boolean) {
-    val dayTemperature = _dayTemperature - KELVIN_CONSTANT
-    val nightTemperature = _nightTemperature - KELVIN_CONSTANT
+class TempDaily1(_dayTemperature: Int, _nightTemperature: Int, _precipitationExpected: Boolean) {
+    val dayTemperature = _dayTemperature - KELVIN_CONSTANT1
+    val nightTemperature = _nightTemperature - KELVIN_CONSTANT1
     val precipitationExpected = _precipitationExpected
 
     fun getWeatherInfo() {
