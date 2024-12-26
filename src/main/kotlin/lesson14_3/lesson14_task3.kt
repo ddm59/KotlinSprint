4e109bd85e5bdfbdb93eafe1b832fd58090cacc5
Dmitry <guilty4all@gmail.com>
2024-12-26 13:43:52 +0600

    KS-14-4 Celestial body class

--- src/main/kotlin/lesson14_3/lesson14_task3.kt
+++ src/main/kotlin/lesson14_3/lesson14_task3.kt
@@ -1,15 +1,17 @@
 package org.example.lesson14_3
+const val BLACK_COLOR: String = "Черный"
+const val  WHITE_COLOR: String = "Белый"
 
 fun main() {
     val figures: List<Figure> = listOf(
-        Circle("Белый", 2.0),
-        Rectangle("Черный", 3.0, 4.0),
-        Circle("Черный", 6.0),
-        Rectangle("Белый", 8.0, 5.0),
+        Circle(WHITE_COLOR, 2.0),
+        Rectangle(BLACK_COLOR, 3.0, 4.0),
+        Circle(BLACK_COLOR, 6.0),
+        Rectangle(WHITE_COLOR, 8.0, 5.0),
     )
 
-    val totalBlackPerimeter = figures.filter { it.color == "Черный" }.sumOf { it.calculatePerimeter() }
-    val totalWhiteArea = figures.filter { it.color == "Белый" }.sumOf { it.calculateArea() }
+    val totalBlackPerimeter = figures.filter { it.color == BLACK_COLOR }.sumOf { it.calculatePerimeter() }
+    val totalWhiteArea = figures.filter { it.color == WHITE_COLOR }.sumOf { it.calculateArea() }
 
     println("Сумма периметров всех черных фигур: $totalBlackPerimeter")
     println("Сумма площадей всех белых фигур: $totalWhiteArea")
