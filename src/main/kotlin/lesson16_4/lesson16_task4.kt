b2e67aee1842f6ed97339b0564f89819dfdcffc7
Dmitry <guilty4all@gmail.com>
2025-01-19 17:27:52 +0600

    KS-16-4 class order

---
+++ src/main/kotlin/lesson16_4/lesson16_task4.kt
@@ -0,0 +1,33 @@
+package org.example.lesson16_4
+
+class Order(private val orderNumber: Int) {
+
+    var status: String = "Создан"
+        private set // Закрываем возможность прямого изменения статуса
+
+    private fun updateStatus(newStatus: String) {
+        status = newStatus
+    }
+
+    fun requestStatusChange(newStatus: String) {
+        println("Отправка заявки менеджеру на изменение статуса заказа #$orderNumber...")
+        managerApproveStatusChange(newStatus)
+    }
+
+    private fun managerApproveStatusChange(newStatus: String) {
+        println("Менеджер одобрил изменение статуса для заказа #$orderNumber.")
+        updateStatus(newStatus)
+        println("Статус заказа #$orderNumber успешно изменён на '$status'.")
+    }
+}
+
+fun main() {
+    val order = Order(12345)
+    println("Текущий статус заказа: ${order.status}")
+
+    order.requestStatusChange("В обработке")
+    println("Текущий статус заказа: ${order.status}")
+
+    order.requestStatusChange("Доставлен")
+    println("Текущий статус заказа: ${order.status}")
+}
