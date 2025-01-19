b0a45894e26b15202487831e6bcb5fe0e605dd41
Dmitry <guilty4all@gmail.com>
2025-01-19 17:14:32 +0600

    KS-15-2 pattern command

--- src/main/kotlin/lesson15_2/lesson15_task2.kt
+++ src/main/kotlin/lesson15_2/lesson15_task2.kt
@@ -1,24 +1,38 @@
 package org.example.lesson16_2
 
+
 abstract class WeatherStationStats {
-    abstract fun getData(): String
+    abstract fun sendData(): String
 }
 
 class Temperature(private val temperature: Double) : WeatherStationStats() {
-    override fun getData(): String {
+    override fun sendData(): String {
         return "Температура: $temperature°C"
     }
 }
 
 class PrecipitationAmount(private val precipitation: Double) : WeatherStationStats() {
-    override fun getData(): String {
+    override fun sendData(): String {
         return "Осадки: $precipitation mm"
     }
 }
 
+interface Command {
+    fun execute()
+}
+
 class WeatherServer {
     fun sendToServer(data: WeatherStationStats) {
-        println("Отправка данных на сервер: ${data.getData()}")
+        println("Отправка данных на сервер: ${data.sendData()}")
+    }
+}
+
+class WeatherDataCommand(
+    private val server: WeatherServer,
+    private val data: WeatherStationStats
+) : Command {
+    override fun execute() {
+        server.sendToServer(data)
     }
 }
 
@@ -28,6 +42,11 @@ fun main() {
 
     val server = WeatherServer()
 
-    server.sendToServer(temperatureData)
-    server.sendToServer(precipitationData)
+    // Создание команд
+    val tempCommand = WeatherDataCommand(server, temperatureData)
+    val precipCommand = WeatherDataCommand(server, precipitationData)
+
+    // Выполнение команд
+    tempCommand.execute()
+    precipCommand.execute()
 }
