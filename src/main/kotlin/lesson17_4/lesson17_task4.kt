22735a9ddfd3d21b49edce73c74e4d12639dc548
Dmitry <guilty4all@gmail.com>
2025-01-20 21:21:36 +0600

    KS-17-4 package class

---
+++ src/main/kotlin/lesson17_4/lesson17_task4.kt
@@ -0,0 +1,37 @@
+package org.example.lesson17_4
+
+class Package(private val packageNumber: String) {
+
+    var currentLocation: String = "Неизвестна"
+        set(value) {
+            field = value
+            movementCounter++
+        }
+
+    companion object {
+        var movementCounter: Int = 0
+            private set
+    }
+
+    fun displayInfo() {
+        println("Номер посылки: $packageNumber")
+        println("Текущая локация: $currentLocation")
+        println("Счетчик передвижений: $movementCounter")
+    }
+}
+
+fun main() {
+
+    val package1 = Package("12345")
+
+    package1.displayInfo()
+
+    package1.currentLocation = "Процессинговый центр A"
+    package1.displayInfo()
+
+    package1.currentLocation = "Процессинговый центр B"
+    package1.displayInfo()
+
+    package1.currentLocation = "Центр доставки"
+    package1.displayInfo()
+}
