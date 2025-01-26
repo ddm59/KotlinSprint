7d82825d7ca58af416d6b0c9f127c9d516b585d3
Dmitry <guilty4all@gmail.com>
2025-01-26 20:16:39 +0600

    KS-21-4 File class extesion fun

---
+++ src/main/kotlin/lesson21_4/lesson21_task4.kt
@@ -0,0 +1,27 @@
+package org.example.lesson21_4
+
+import java.io.File
+
+
+fun File.prependText(word: String) {
+
+    val lowerCaseWord = word.lowercase()
+
+    val existingContent = if (this.exists()) this.readText() else ""
+
+    val newContent = lowerCaseWord + "\n" + existingContent
+
+    this.writeText(newContent)
+}
+
+fun main() {
+
+    val file = File("words.txt")
+
+    file.prependText("Hello")
+    file.prependText("World")
+    file.prependText("Kotlin")
+
+    println("Содержимое файла:")
+    println(file.readText())
+}
