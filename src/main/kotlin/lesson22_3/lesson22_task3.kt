073686c791ef35756966086529a40154ad895352
Dmitry <guilty4all@gmail.com>
2025-01-29 19:50:51 +0600

    KS-22-3 mvvm imitation

--- src/main/kotlin/lesson22_3/lesson22_task3.kt
+++ src/main/kotlin/lesson22_3/lesson22_task3.kt
@@ -1,7 +1,7 @@
 data class DataBookCopy(val title: String, val author: String, val year: Int)
 
 fun main() {
-    val dataBook = DataBookCopy("1984", "George Orwell", 1949)
+    val dataBook = DataBookCopy("1984 ", "George Orwell", 1949)
 
     println("Вывод DataBookCopy: $dataBook")
 
