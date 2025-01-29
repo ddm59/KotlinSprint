bacb66ef73f75ff92182f2746a79c48a246cbba7
Dmitry <guilty4all@gmail.com>
2025-01-29 19:56:37 +0600

    KS-22-5 galactic guide

---
+++ src/main/kotlin/lesson22_5/lesson22_task5.kt
@@ -0,0 +1,25 @@
+package org.example.lesson22_5
+
+import java.time.LocalDateTime
+
+data class GalacticGuide(
+    val name: String,
+    val description: String,
+    val eventDateTime: LocalDateTime,
+    val distanceFromEarth: Double
+)
+
+fun main() {
+
+    val alphaCentauri = GalacticGuide(
+        name = "Alpha Centauri",
+        description = "Ближайшая звездная система к Солнечной системе, состоящая из трех звезд: Alpha Centauri A, Alpha Centauri B и Proxima Centauri.",
+        eventDateTime = LocalDateTime.now(),
+        distanceFromEarth = 4.37
+    )
+
+    println("Название: ${alphaCentauri.component1()}")
+    println("Описание: ${alphaCentauri.component2()}")
+    println("Дата и время события: ${alphaCentauri.component3()}")
+    println("Расстояние от Земли: ${alphaCentauri.component4()} световых лет")
+}
