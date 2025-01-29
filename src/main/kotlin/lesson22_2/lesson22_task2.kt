942648c3eb79f9efd4a23b77aa9da6f53143becf
Dmitry <guilty4all@gmail.com>
2025-01-29 16:08:16 +0600

    KS-22-2 class comparison toString

---
+++ src/main/kotlin/lesson22_2/lesson22_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson22_2
+
+class RegularBookCopy(val title: String, val author: String)
+
+data class DataBookCopy(val title: String, val author: String)
+
+fun main() {
+    val book = RegularBookCopy("1984", "George Orwell")
+    val dataBook = DataBookCopy("1984", "George Orwell")
+
+    // Вывод объектов в консоль
+    println("Вывод RegularBookCopy: $book")
+    println("Вывод DataBookCopy: $dataBook")
+}
+/*
+   Разница в выводе:
+   - Обычный класс не переопределяет метод toString(), поэтому выводится ссылка на объект.
+   - Data class автоматически генерирует метод toString(), который выводит значения полей.
+*/
